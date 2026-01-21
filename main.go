59f2103d960a6abeb86a5d400f23407f020e1649
mrestuab <mrestuab54@gmail.com>
2026-01-21 09:21:23 +0700

    ganti link deploy

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://frontend-medislink.vercel.app",
+		AllowOrigins: "https://frontend-medislink-cyan.vercel.app",
 
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 
